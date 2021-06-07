6ef7e0c79099de67d67a0b49b55f6daefdcdfd5e
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-06 19:49:02 -0700

    Initial Commit

---
+++ hiitfit-app/hiitfit_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  hiitfit_appApp.swift
+//  hiitfit-app
+//
+//  Created by user on 6/6/21.
+//
+
+import SwiftUI
+
+@main
+struct hiitfit_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
